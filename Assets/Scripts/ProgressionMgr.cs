a1ff460cc2729f2555928e65bad983a7d42cd4ef
Crimson <crimsonsreality@gmail.com>
2022-03-25 19:51:36 -0400

    Setting up test scene

--- Assets/Scripts/ProgressionMgr.cs
+++ Assets/Scripts/ProgressionMgr.cs
@@ -2,20 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
+public struct PlayPauseInfo 
+{
+    public Sprite pause;
+    public Sprite play;
+}
 public class ProgressionMgr : MonoBehaviour
 {
     public static ProgressionMgr instance;
 
-    [Header("Data Objects")]
-    //public List<TutorialData> Tutorials = new List<TutorialData>();
-    public Transform spawnPoint;
-
     [Header("Tutorial Objects")]
     public VolumetricPlayer volumetricPlayer;
+    public GameObject extraVolumetricPlayers;
+
 
     [Header("UI Objects")]
     public GameObject mainMenuPanel;
-    public List<GameObject> contentPanels = new List<GameObject>();
+    public GameObject tutorialPanel;
+    public SpriteRenderer playPausePanel;
+    public PlayPauseInfo playPause;
+    public SpriteRenderer contentPoster;
+    public List<Sprite> contentPanels = new List<Sprite>();
 
     private void Awake()
     {
@@ -29,20 +37,36 @@ public class ProgressionMgr : MonoBehaviour
             instance = this;
         }
     }
+
+    private void Start()
+    {
+        mainMenuPanel.SetActive(true);
+        tutorialPanel.SetActive(false);
+    }
+
     public void StartTutorial(int whichTutorial = 0)
     {
         Debug.Log("Starting Tutorial in Progression MGR");
-        
+        mainMenuPanel.SetActive(false);
+        tutorialPanel.SetActive(true);
+        volumetricPlayer.OnStepChanged.AddListener(SetUIElement);
+        volumetricPlayer.OnStepChanged.AddListener(StepChanged);
+        volumetricPlayer.CurStep = 0;
+        SetUIElement();
         //Tutorials[whichTutorial].PlayTutorial();
     }
 
-    public void SetUIElement()
+    public void StepChanged(int i = 0)
     {
-        foreach (GameObject i in contentPanels)
-        { 
-            
-        }
+
     }
+    public void SetUIElement(int i = 0)
+    {
+        if (contentPanels[i])
+            contentPoster.sprite = contentPanels[i];
+    }
+
+
 
 
 }
